3e45536e09964ee5e8848e40bd98b6aba81988f8
FpRezDev <fprez.developer@outlook.com>
2020-05-23 15:42:18 -0600

    create tests

--- test/test-gulp-node-env.js
+++ test/test-gulp-node-env.js
@@ -12,14 +12,32 @@ describe('gulp-node-env', () => {
       expect(process.env.NODE_ENV).to.equal('development');
     });
 
+    it('should callback after setting environment to development', () => {
+      var wasCalledBack = false;
+      env.SetDevelopmentEnvironment(() => { wasCalledBack = true; });
+      expect(wasCalledBack).to.be.true;
+    });
+
     it('should set environment = production', () => {
       env.SetProductionEnvironment();
       expect(process.env.NODE_ENV).to.equal('production');
     });
 
+    it('should callback after setting environment to production', () => {
+      var wasCalledBack = false;
+      env.SetProductionEnvironment(() => { wasCalledBack = true; });
+      expect(wasCalledBack).to.be.true;
+    });
+
     it('should set environment = test', () => {
       env.SetTestEnvironment();
       expect(process.env.NODE_ENV).to.equal('test');
+    });
+    
+    it('should callback after setting environment to test', () => {
+      var wasCalledBack = false;
+      env.SetTestEnvironment(() => { wasCalledBack = true; });
+      expect(wasCalledBack).to.be.true;
     });
 
     it('should return true from IsDevelopment()', () => {
